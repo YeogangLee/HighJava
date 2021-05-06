8abaffa77e3c6a9d4c2b1072a5726c6ca9a86854
DESKTOP-2SBVSE1\PC-14 <coffee357722@gmail.com>
2021-05-06 21:19:54 +0900

    코드 수정

--- ThreadTest/src/kr/or/ddit/basic/T15_SyncThreadTest.java
+++ ThreadTest/src/kr/or/ddit/basic/T15_SyncThreadTest.java
@@ -37,15 +37,26 @@ public class T15_SyncThreadTest {
 class ShareObject {
 	private int sum = 0;
 	
-	//동기화 처리를 위해 synchronized 키워드 추가
-	synchronized public void add() {
-		for(int i=0; i<1000000000; i++) {} //동기화처리 전까지 시간 벌기용
+	//동기화 방법1: synchronized 키워드 추가, 메서드 자체에 동기화 설정
+	//어떤 스레드가 동기화 메서드에 진입하는 순간, 다른 스레드들은 공유객체에 접근 불가
+//	synchronized public void add() {
+	public void add() {
 		
-		int n = sum;
-		n += 10; //10 증가
-		sum = n;
+		//동기화 방법2: 동기화 블럭 설정
+		//현재는 전체 영역 동기화 - 방법1 동기화 메서드와 비슷하다
 		
-		System.out.println(Thread.currentThread().getName() + "합계 : " + sum);
+		//동기화 메서드는 동기화 영역이 메서드 전체
+		//동기화 블럭은 동기화 영역 조정 가능 but 영역을 벗어나는 순간 동기화가 풀린다
+		//동기화 블럭의 영역은 최소화 시키는 게 좋다, 영역을 넓히면 동기화가 확실해서 좋을 순 있어도 속도가 느려진다.
+		synchronized (this) {
+			for(int i=0; i<1000000000; i++) {} //동기화처리 전까지 시간 벌기용
+			
+			int n = sum;
+			n += 10; //10 증가
+			sum = n;
+			
+			System.out.println(Thread.currentThread().getName() + "합계 : " + sum);
+		}
 	}
 	
 	public int getSum() {
